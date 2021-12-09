d8fd9cb913bb61d6924635f6f08823d0e23949b2
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2021-12-08 19:58:50 -0500

    populate billing, credit forms

--- src/components/Profile/BillingForm/BillingForm.jsx
+++ src/components/Profile/BillingForm/BillingForm.jsx
@@ -2,10 +2,60 @@ import "./BillingForm.scss";
 
 const BillingForm = () => {
   return (
-    // add form fields
-    <div className="form billing-form">
-      <div className="form-heading font-face-grotesque">Billing Information</div>
+    <div className="form billing-form font-face-grotesque">
+      <div className="form-heading">Client Profile</div>
+      <div className="input-wrapper">
+        <label for="contact-name">Contact Name </label>
+        <input type="text" name="contact-name" id="contact-name" placeholder="Contact Name" />
+      </div>
+      <div className="input-wrapper">
+        <label for="contact-address">Address</label>
+        <input type="text" name="contact-address" id="contact-address" placeholder="Address" />
+      </div>
+
+      <div className="two-col">
+        <div className="input-wrapper">
+          <label for="apt">Apartment, suite, etc</label>
+          <input type="text" name="apt" id="apt" placeholder="Apartment, suite, etc"/>   
+        </div>
+        <div className="input-wrapper">
+          <label for="country">Country</label>
+          <select name="country" id="country">
+            <option value="usa">United States</option>
+            <option value="canada">Canada</option>
+            <option value="france">France</option>
+            <option value="italy">Italy</option>
+            <option value="japan">Japan</option>
+            <option value="united-kingdom">United Kingdom</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="two-col">
+        <div className="input-wrapper">
+          <label for="state">State</label>
+          <select name="state" id="state" placeholder="State">
+            <option value="new-york">New York</option>
+            <option value="new-jersey">New Jersey</option>
+            <option value="connecticut">Connecticut</option>
+            <option value="pennsylvania">Pennsylvania</option>
+          </select>
+        </div>
+        <div className="input-wrapper">
+          <label for="">Zip Code</label>
+          <input type="text" name="zip" id="zip" placeholder="Zip Code"/>   
+        </div>
+      </div>
       
+      <div className="input-wrapper">
+        <label for="contact-email">Email</label>
+        <input type="text" name="contact-email" id="contact-email" placeholder="Email" />
+      </div>
+      <div className="input-wrapper">
+        <label for="contact-address">Phone</label>
+        <input type="tel" name="phone" id="phone" 
+               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone"/>
+      </div>
     </div>
   )
 }
--- src/components/Profile/CreditForm/CreditForm.jsx
+++ src/components/Profile/CreditForm/CreditForm.jsx
@@ -2,15 +2,52 @@ import "./CreditForm.scss";
 
 const CreditForm = () => {
   return (
-    // add form fields
-    <div className="form credit-form">
-      <div className="form-heading font-face-grotesque">Card On File</div>
-      <form action="" method="">
-        <div>
-          <label for="name">*Name </label>
-          <input type="text" name="name" id="name" required/>
+    <div className="form credit-form font-face-grotesque">
+      <div className="form-heading">Card On File</div>
+      <div className="input-wrapper">
+        <label for="cc">Credit Card </label>
+        <input type="number" name="cc" id="cc" placeholder="Credit Card" />
+      </div>
+
+      <div className="two-col">
+        <div className="input-wrapper">
+          <label for="cc-firstName">First Name </label>
+          <input type="text" name="cc-firstName" id="cc-firstName" placeholder="First Name" />
         </div>
-      </form>
+        <div className="input-wrapper">
+          <label for="cc-lastName">Last Name </label>
+          <input type="text" name="cc-lastName" id="cc-lastName" placeholder="Last Name" />
+        </div>
+      </div>
+
+      <div className="three-col">
+        <div className="input-wrapper">
+          <label for="month">Month</label>
+          <select name="month" id="month" placeholder="Month">
+            <option value="january">January</option>
+            <option value="february">February</option>
+            <option value="march">March</option>
+            <option value="april">April</option>
+            <option value="may">May</option>
+            <option value="june">June</option>
+            <option value="july">July</option>
+            <option value="august">August</option>
+            <option value="september">September</option>
+            <option value="october">October</option>
+            <option value="november">November</option>
+            <option value="december">December</option>
+          </select>
+        </div>
+        <div className="input-wrapper">
+          <label for="year">Year</label>
+          <input type="number" name="year" id="year" placeholder="Year"/>   
+        </div>
+        <div className="input-wrapper">
+          <label for="cvv">CVV</label>
+          <input type="number" name="cvv" id="cvv" placeholder="CVV"/>
+        </div>
+      </div>
+
     </div>
   )
 }
