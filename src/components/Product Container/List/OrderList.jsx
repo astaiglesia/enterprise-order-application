fad6d68eb8cd0f635246d4ae2f9ee1aac7e01349
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2021-12-11 14:38:52 -0500

    add Vue refactoring notes

--- src/components/Product Container/List/OrderList.jsx
+++ src/components/Product Container/List/OrderList.jsx
@@ -3,20 +3,43 @@ import "./OrderList.scss";
 const OrderList = () => {
   return (
     <div className="order-list">
-      - create and import item component
-      - declare initial state with an empty itemList array
+      - create and import list item component
+      - declare initial state with an empty itemList array of product-data-objects
       - onClick of additem to: 
           * validate item in database 
-          * fetch product data
-          * dynamically populates an item componet 
-          * push to itemList with the queried item
-      - product pushed to itemList triggers re-render of component
-      - 
-      <div className="temp">Item</div>
-      <div className="temp">Item</div>
-      <div className="temp">Item</div>
+          * fetch product data-object
+          * push object to itemList for the queried item
+      - iterate through state list to render an li item compnent passing in object for component state
+      - view re-renders on state change
+
+      <ul>
+        <li className="temp">
+          List Item Component placeholder
+        </li>
+      </ul>
+      
+      
     </div>
   )
 }
 
 export default OrderList;
+
+// - in Vue - refactor to
+// const listItem = {
+//   data() {
+//     return {
+//       items: [
+//         'object1',
+//         'object2',
+//         'object3'
+//       ]
+//     }
+//   }
+// }
+// <div id="listItem"> 
+//   <li v-for="item in items">
+//     {{ item }}
+//   </li>
+// </div>
+
