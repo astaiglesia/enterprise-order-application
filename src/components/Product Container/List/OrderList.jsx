cae652c1710d81c6732229a074ad416d1ee2dbf5
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2021-12-11 14:11:54 -0500

    add pseudocode

--- src/components/Product Container/List/OrderList.jsx
+++ src/components/Product Container/List/OrderList.jsx
@@ -3,6 +3,15 @@ import "./OrderList.scss";
 const OrderList = () => {
   return (
     <div className="order-list">
+      - create and import item component
+      - declare initial state with an empty itemList array
+      - onClick of additem to: 
+          * validate item in database 
+          * fetch product data
+          * dynamically populates an item componet 
+          * push to itemList with the queried item
+      - product pushed to itemList triggers re-render of component
+      - 
       <div className="temp">Item</div>
       <div className="temp">Item</div>
       <div className="temp">Item</div>
