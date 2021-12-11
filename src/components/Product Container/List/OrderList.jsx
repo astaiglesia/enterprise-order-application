a90f3effe718c3ded9666c9d9d7aae59fc6e7017
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2021-12-11 16:40:09 -0500

    edit Vue migration notes

--- src/components/Product Container/List/OrderList.jsx
+++ src/components/Product Container/List/OrderList.jsx
@@ -25,18 +25,11 @@ const OrderList = () => {
 
 export default OrderList;
 
-// - in Vue - refactor to
-// const listItem = {
-//   data() {
-//     return {
-//       items: [
-//         'object1',
-//         'object2',
-//         'object3'
-//       ]
-//     }
-//   }
-// }
+// - refactor list and dynamic component in Vue 
+// use v-for :key and v-model (dynamically populating ul's) => relevant example in Sarah Drasner's CodePen -> https://codepen.io/sdras/pen/84ebef8c6b53973904071a1303958b31
+//
+// use v-on || @ directive as an event binder (button actions) => https://frontendmasters.com/courses/vue-3/event-bindings-v-on/
+//
 // <div id="listItem"> 
 //   <li v-for="item in items">
 //     {{ item }}
